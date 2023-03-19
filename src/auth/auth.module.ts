4b9b8eab2c08d467bd08105156e34d9116c2bb89
Nadya Selickaya <nadinehm2210@mail.ru>
2023-03-19 14:12:02 +0400

    jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,31 +1,31 @@
-import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { AuthController } from './auth.controller';
-import { TypegooseModule } from 'nestjs-typegoose';
-import { UserModel } from 'src/user/user.model';
-import { ConfigModule, ConfigService } from '@nestjs/config';
-import { JwtModule } from '@nestjs/jwt';
-import { getJWTConfig } from 'src/config/jwt.config';
-import { JWTStrategy } from './strategies/jwt.strategy';
+import { Module } from '@nestjs/common'
+import { ConfigModule, ConfigService } from '@nestjs/config'
+import { JwtModule } from '@nestjs/jwt'
+import { TypegooseModule } from 'nestjs-typegoose'
+import { getJWTConfig } from 'src/config/jwt.config'
+import { JwtStrategy } from './strategies/jwt.strategy'
+import { AuthController } from './auth.controller'
+import { UserModel } from '../user/user.model'
+import { AuthService } from './auth.service'
 
 @Module({
-  controllers: [AuthController],
-  imports:[
-    TypegooseModule.forFeature([
-     {
-        typegooseClass: UserModel,
-        schemaOptions:{
-          collection:'User'
-        },
-     },
-    ]),
-    ConfigModule,
-    JwtModule.registerAsync({
-      imports:[ConfigModule],
-      inject:[ConfigService],
-      useFactory:getJWTConfig
-    })
-  ],
-  providers: [AuthService, JWTStrategy],
+	controllers: [AuthController],
+	imports: [
+		TypegooseModule.forFeature([
+			{
+				typegooseClass: UserModel,
+				schemaOptions: {
+					collection: 'User',
+				},
+			},
+		]),
+		ConfigModule,
+		JwtModule.registerAsync({
+			imports: [ConfigModule],
+			inject: [ConfigService],
+			useFactory: getJWTConfig,
+		}),
+	],
+	providers: [AuthService, JwtStrategy],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -15,7 +15,13 @@ export class AuthService {
     ){}
 
     async login(dto:AuthDto){
-        return this.validateUser(dto)
+        const user = await this.validateUser(dto)
+        const tokens = await this.issueTokenPair(String(user._id))
+    
+        return {
+            user: this.returnUserFields(user),
+            ...tokens
+        }
     }
 
     async register(dto:AuthDto){
@@ -30,7 +36,13 @@ export class AuthService {
             password: await hash(dto.password, salt)
         }
         )
-        return newUser.save()
+
+        const tokens = await this.issueTokenPair(String(newUser._id))
+    
+        return {
+            user: this.returnUserFields(newUser),
+            ...tokens
+        }
     }
 
     async validateUser(dto:AuthDto):Promise<UserModel>{
