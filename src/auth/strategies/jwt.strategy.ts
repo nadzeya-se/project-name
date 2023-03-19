f07f8ab1ecd77b9616159b97a3b5f8daee8e611e
Nadya Selickaya <nadinehm2210@mail.ru>
2023-03-19 14:10:14 +0400

    jwt

--- src/auth/strategies/jwt.strategy.ts
+++ src/auth/strategies/jwt.strategy.ts
@@ -1,24 +1,27 @@
-import { Injectable } from "@nestjs/common";
-import { ConfigService } from "@nestjs/config";
-import { PassportStrategy } from "@nestjs/passport";
-import { InjectModel } from "nestjs-typegoose";
-import { ExtractJwt, Strategy } from "passport-jwt";
-import { UserModel } from "src/user/user.model";
-import { ModelType } from "typegoose";
-
+import { Injectable } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { PassportStrategy } from '@nestjs/passport'
+import { ModelType } from '@typegoose/typegoose/lib/types'
+import { InjectModel } from 'nestjs-typegoose'
+import { ExtractJwt, Strategy } from 'passport-jwt'
+import { UserService } from 'src/user/user.service'
+import { UserModel } from '../../user/user.model'
 
 @Injectable()
-export class JWTStrategy extends PassportStrategy(Strategy){
-constructor(private readonly configService: ConfigService, @InjectModel(UserModel) private readonly UserModel:ModelType<UserModel>){
-super({
-    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-    ignoreExpiration:true,
-    secretKey: configService.get('JWT_SECRET')
-})
-}
+export class JwtStrategy extends PassportStrategy(Strategy) {
+	constructor(
+		private readonly configService: ConfigService,
+		@InjectModel(UserModel) private readonly UserModel: ModelType<UserModel>
+	) {
+		super({
+			jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+			ignoreExpiration: true,
+			secretOrKey: configService.get('JWT_SECRET'),
+		})
+	}
 
-async validate({_id}:Pick<UserModel, '_id'>){
-    return this.UserModel.findById(_id).exec()
-}
-
+	async validate({ _id }: Pick<UserModel, '_id'>) {
+		const user = await this.UserModel.findById(_id)
+		return user
+	}
 }
